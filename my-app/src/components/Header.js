483b5e1378112d82980b685d4dad2811c4905d5c
GITPSA28 <74608471+GITPSA28@users.noreply.github.com>
2021-09-11 12:16:40 +0530

    NotesApp
    First sample test app with dummy data

---
+++ my-app/src/components/Header.js
@@ -0,0 +1,13 @@
+
+import '../styles/Header.css'
+
+const Header=()=>{
+    return(
+        <div className="app-header">
+        <h1> Notes app</h1>
+        </div>
+    )
+
+}
+
+export default Header;
---
+++ my-app/src/pages/NotesListPage.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import notes from '../assets/data'
+import Listitem from '../components/Listitem'
+const NotesListPage = () => {
+    return (
+        <div className="notes">
+
+            <div className="notes-header">
+                <h2 className="notes-title">&#9782; Notes</h2>
+                <p className="notes-count">{notes.length}</p>
+            </div>
+
+            <div className="notes-list">
+                {notes.map((note,index)=>(
+                    <Listitem key={index} note={note}/>
+                   
+                )
+                   
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default NotesListPage
